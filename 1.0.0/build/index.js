73c8db0ff9836979c0447019b5b21abf10128270
kissyteam <tiehang.lth@taobao.com>
2015-04-21 10:59:14 +0800

    sync daily version 1.0.0

--- 1.0.0/build/index.js
+++ 1.0.0/build/index.js
@@ -3,69 +3,75 @@ KISSY.add('kg/effect-paginations/1.0.0/index',["node","base"],function(S ,requir
 var Base = require('base');
 
 var EffectPaginations = Base.extend({
+
     initializer: function(){
+
         var self = this;
         var $target = self.get('$target');
+        var container$ = $(self.get('boxHook'));
+        self._moveByMouse(container$);
+
+        return self
+
     },
-    _render: function(){
+
+    _moveByMouse: function(container$, callback){
 
         var self = this;
-        var container$ = self.$boxHook;
 
-        moveByMouse(container$);
+        var pageAnimateTimer = false,
+            moveByMouseInit = false;
+        var curPageItem$ = $(self.get('curChildHook')),
+            itemWithMask$ = null;
 
-        function moveByMouse(container$, callback){
-            var pageAnimateTimer = false,
-                moveByMouseInit = false;
-            var curPageSpan$ = $('.page-cur'),
-                curPageItem$ = curPageSpan$.parent('.item'),
-                itemWithMask$ = null;
-            container$.delegate('mouseenter', '.item', function(ev){
-                pageAnimateTimer && pageAnimateTimer.stop(false);
-                var currentItem$ = $(ev.currentTarget);
-                var endX = currentItem$.offset().left - container$.offset().left,
-                    endWidth = currentItem$.width(),
-                    mask$ = null;
-                if(!moveByMouseInit){
-                    moveByMouseInit = true;
-                    container$.css('position', 'relative');
-                    var initX = curPageItem$.offset().left - container$.offset().left,
-                        width = curPageItem$.width(),
-                        height = curPageItem$.height();
-                    mask$ = $('<div id="mask" style="border:2px solid #6D3053;z-index:999;position:absolute;width:' + width +'px;height:' + height +'px;left:' + initX +'px"></div>');
-                    container$.prepend(mask$);
-                }else{
-                    mask$ = container$.all('#mask');
+        // 动效开始
+        container$.delegate('mouseenter', self.get('childHook'), function(ev){
+            pageAnimateTimer && pageAnimateTimer.stop(false);
+            var currentItem$ = $(ev.currentTarget);
+            var endX = currentItem$.offset().left - container$.offset().left,
+                endWidth = currentItem$.width(),
+                mask$ = null;
+            if(!moveByMouseInit){
+                moveByMouseInit = true;
+                container$.css('position', 'relative');
+                var initX = curPageItem$.offset().left - container$.offset().left,
+                    width = curPageItem$.width(),
+                    height = curPageItem$.height();
+                mask$ = $('<div id="mask" style="border:2px solid #6D3053;z-index:999;position:absolute;width:' + width +'px;height:' + height +'px;left:' + initX +'px"></div>');
+                container$.prepend(mask$);
+            }else{
+                mask$ = container$.all('#mask');
+            }
+            pageAnimateTimer = mask$.animate({
+                left : endX,
+                width : endWidth
+            },{
+                duration : 0.1,
+                complete : function(){
+                    mask$.css('z-index','1');
+                    itemWithMask$ = currentItem$;
                 }
-                pageAnimateTimer = mask$.animate({
-                    left : endX,
-                    width : endWidth
-                },{
-                    duration : 0.1,
-                    complete : function(){
-                        mask$.css('z-index','1');
-                        itemWithMask$ = currentItem$;
-                    }
-                });
             });
+        });
 
-            container$.delegate('mouseleave', '.item', function(ev){
-                pageAnimateTimer.isRunning() && pageAnimateTimer.stop(true);
+        // 动效结束
+        container$.delegate('mouseleave', '.item', function(ev){
+            pageAnimateTimer.isRunning() && pageAnimateTimer.stop(true);
 
-                var currentTarget$ = $(ev.currentTarget);
-                var endX = curPageItem$.offset().left - container$.offset().left,
-                    mask$ = container$.all('#mask');
-                pageAnimateTimer = mask$.animate({
-                    left : endX
-                },{
-                    duration : 0.1,
-                    complete : function(){
-                        mask$.remove();
-                        moveByMouseInit = false;
-                    }
-                });
+            var currentTarget$ = $(ev.currentTarget);
+            var endX = curPageItem$.offset().left - container$.offset().left,
+                mask$ = container$.all('#mask');
+            pageAnimateTimer = mask$.animate({
+                left : endX
+            },{
+                duration : 0.1,
+                complete : function(){
+                    mask$.remove();
+                    moveByMouseInit = false;
+                }
             });
-        }
+        });
+
     }
 },{
     ATTRS:{
@@ -75,8 +81,14 @@ var EffectPaginations = Base.extend({
                 return $(v);
             }
         },
-        $boxHook: {
+        boxHook: {
             value: '.pagination-page'
+        },
+        childHook: {
+            value: '.item'
+        },
+        curChildHook: {
+            value: '.item-sel'
         }
     }
 });
--- 1.0.0/guide/index.md
+++ 1.0.0/guide/index.md
@@ -1,11 +1,25 @@
 ## 综述
 
-EffectPaginations。
+EffectPaginations。  
+淘宝动效分页组件，仅实现动效，不关心分页逻辑。
 
 ## 初始化组件
 		
-    S.use('kg/effect-paginations/1.0.0/index', function (S, EffectPaginations) {
-         var effect-paginations = new EffectPaginations();
+    S.use('kg/effect-paginations/1.0.0/index,kg/effect-paginations/1.0.0/index.css', function (S, EffectPaginations) {
+        new EffectPaginations({
+            boxHook: '.pagination-page',
+            childHook: '.item',
+            curChildHook: '.item-sel'
+        });
     })
 
 ## API说明
+
+组件不依赖指定的分页dom结构。  
+使用组件时需要指定如下参数
+  
+  ＊ boxHook 组件容器钩子，用作页面代理
+  ＊ childHook 单个分页按钮钩子
+  ＊ curChildHook 选中分页按钮钩子
+
+注：如果页面有多个分页组件，建议每个分页组件用不同钩子区分，初始化多个 EffectPaginations
